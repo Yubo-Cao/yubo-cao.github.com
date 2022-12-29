92c5eda043d21429b698f35b39c922c668cbe010
Yubo Cao <cao2006721@gmail.com>
2022-12-29 18:36:34 -0500

    feat: add hover style

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
+import { cls } from "../lib/utils";
 import Icon from "./Icon";
 
-export default function MenuButton(props: {
+export default function MenuButton({
+    open,
+    onClick,
+    size = 24
+}: {
     open: boolean;
     onClick: () => void;
     size?: number;
     className?: string;
 }) {
-    const { open, onClick } = props;
-    let { className, size } = props;
-    className = className || "";
+    let [entered, setEntered] = useState(false);
 
     return (
         <div
             onClick={onClick}
-            className={`transition-all rounded-full active:border active:border-black active:bg-neutral-100 flex justify-center items-center w-12 h-12 ${className}`}
+            onMouseEnter={() => setEntered(true)}
+            onMouseLeave={() => setEntered(false)}
+            className={cls(
+                "transition-all",
+                "rounded-full",
+                "active:border",
+                "active:border-black",
+                "active:bg-slate-100",
+                "hover:bg-slate-100",
+                "flex",
+                "justify-center",
+                "items-center",
+                "w-12",
+                "h-12"
+            )}
         >
             <Icon
                 from="md"
                 name={open ? "menu_open" : "menu"}
                 size={size}
                 className="transition-all"
+                grade={entered ? 200 : 0}
             ></Icon>
         </div>
     );

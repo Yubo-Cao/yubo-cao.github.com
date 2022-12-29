f5e2cba90e51e009f78468ba42ede23a07870f5c
Yubo Cao <cao2006721@gmail.com>
2022-12-29 11:19:20 -0500

    refact: extract a blog card components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,24 @@ import React from "react";
 import "../styles/globals.css";
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import localFont from "@next/font/local";
 
 const inter = Inter({
     subsets: ["latin"],
     variable: "--font-inter"
+});
+
+const materialSymbolsRounded = localFont({
+    src: "./fonts/MaterialSymbolsRounded.woff2",
+    variable: "--font-material-symbols-rounded"
+});
+const materialSymbolsOutlined = localFont({
+    src: "./fonts/MaterialSymbolsOutlined.woff2",
+    variable: "--font-material-symbols-outlined"
+});
+const materialSymbolsSharp = localFont({
+    src: "./fonts/MaterialSymbolsSharp.woff2",
+    variable: "--font-material-symbols-sharp"
 });
 
 interface AppProps {
@@ -23,7 +37,9 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             </Head>
-            <div className={`${inter.variable} font-sans`}>
+            <div
+                className={`${inter.variable} ${materialSymbolsRounded.variable} ${materialSymbolsOutlined.variable} ${materialSymbolsSharp.variable} font-sans`}
+            >
                 <Component {...pageProps} />
             </div>
         </>
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -9,101 +9,94 @@ import HeaderLayout from "../../components/HeaderLayout";
 import { cls } from "../../lib/utils";
 import Image from "next/image";
 
-export default function Index({ blogs }: { blogs: BlogPost[] }) {
-    let tags: { [key: string]: BlogPost[] } = {};
-    blogs.forEach((blog) => {
-        blog.tags.forEach((tag) => {
-            if (tags[tag]) tags[tag].push(blog);
-            else tags[tag] = [blog];
-        });
-    });
-
+function BlogCard(props: { blog: BlogPost }) {
     let router = useRouter();
+    return (
+        <Card
+            onClick={() => router.push(props.blog.url)}
+            hoverType={"elevated"}
+            activeType={"elevated"}
+            className={cls("p-0", "max-w-2xl")}
+        >
+            <section
+                className={cls(
+                    "w-full",
+                    "h-full",
+                    "flex",
+                    "flex-col",
+                    "md:flex-row",
+                    "md:gap-4"
+                )}
+            >
+                <div className={cls("overflow-hidden", "md:h-full", "md:w-1/3")}>
+                    <Image
+                        src={props.blog.cover}
+                        width={300}
+                        height={200}
+                        alt={`Cover image for ${props.blog.title}`}
+                        className={cls(
+                            "rounded-t-lg",
+                            "w-full",
+                            "h-full",
+                            "object-cover",
+                            "md:rounded-t-none",
+                            "md:rounded-l-lg"
+                        )}
+                    />
+                </div>
+                <div
+                    className={cls(
+                        "p-4",
+                        "md:w-2/3",
+                        "flex-auto",
+                        "flex",
+                        "justify-between",
+                        "flex-col"
+                    )}
+                >
+                    <Title
+                        level={3}
+                        title={props.blog.title}
+                        subtitle={props.blog.abstract}
+                        subtitleClassName={cls("text-slate-500", "text-lg", "font-semibold")}
+                        className={cls("mb-0")}
+                    />
+                    <div className={cls("mt-4", "space-y-2")}>
+                        <p className={cls("text-sm", "text-gray-500")}>{props.blog.date}</p>
+                        <div className={cls("flex", "flex-row", "gap-2")}>
+                            <Icon name={"tag"} className={cls("text-gray-500")} size={24} />
+                            <div className={`space-x-2`}>
+                                {props.blog.tags.map((tag) => (
+                                    <Chip key={tag}>{tag}</Chip>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Card>
+    );
+}
 
+export default function Index({ blogs }: { blogs: BlogPost[] }) {
     return (
         <HeaderLayout active={"blog"}>
-            {Object.entries(tags).map(([tag, blogs]) => (
-                <Section title={tag} flow={true} key={tag}>
-                    {blogs.map((blog) => (
-                        <Card
-                            onClick={() => router.push(blog.url)}
-                            key={blog.id.join("-")}
-                            hoverType={"elevated"}
-                            activeType={"elevated"}
-                            className={cls("p-0", "max-w-2xl")}
-                        >
-                            <section
-                                className={cls(
-                                    "w-full",
-                                    "h-full",
-                                    "flex",
-                                    "flex-col",
-                                    "md:flex-row",
-                                    "md:gap-4"
-                                )}
-                            >
-                                <div
-                                    className={cls("overflow-hidden", "md:h-full", "md:w-1/3")}
-                                >
-                                    <Image
-                                        src={blog.cover}
-                                        width={300}
-                                        height={200}
-                                        alt={`Cover image for ${blog.title}`}
-                                        className={cls(
-                                            "rounded-t-lg",
-                                            "w-full",
-                                            "h-full",
-                                            "object-cover",
-                                            "md:rounded-t-none",
-                                            "md:rounded-l-lg"
-                                        )}
-                                    />
-                                </div>
-                                <div
-                                    className={cls(
-                                        "p-4",
-                                        "md:w-2/3",
-                                        "flex-auto",
-                                        "flex",
-                                        "justify-between",
-                                        "flex-col"
-                                    )}
-                                >
-                                    <Title
-                                        level={3}
-                                        title={blog.title}
-                                        subtitle={blog.abstract}
-                                        subtitleClassName={cls(
-                                            "text-slate-500",
-                                            "text-lg",
-                                            "font-semibold"
-                                        )}
-                                        className={cls("mb-0")}
-                                    />
-                                    <div className={cls("mt-4", "space-y-2")}>
-                                        <p className={cls("text-sm", "text-gray-500")}>
-                                            {blog.date}
-                                        </p>
-                                        <div className={cls("flex", "flex-row", "gap-2")}>
-                                            <Icon
-                                                name={"tag"}
-                                                className={cls("text-gray-500")}
-                                                size={24}
-                                            />
-                                            <div className={`space-x-2`}>
-                                                {blog.tags.map((tag) => (
-                                                    <Chip key={tag}>{tag}</Chip>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </section>
-                        </Card>
+            <Section title={"Recent"} flow={true}>
+                {blogs
+                    .filter(
+                        (blog) =>
+                            (Date.now() - new Date(blog.date).getTime()) / 1000 / 60 / 60 / 24 <
+                            7
+                    )
+                    .map((blog) => (
+                        <BlogCard key={blog.id.join("-")} blog={blog} />
                     ))}
-                </Section>
-            ))}
+            </Section>
+            <Section title={"All"} flow={true}>
+                {blogs.map((blog) => (
+                    <BlogCard key={blog.id.join("-")} blog={blog} />
+                ))}
+            </Section>
         </HeaderLayout>
     );
 }

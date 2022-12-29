6de841200338ad4c53402998530559b12a5a5562
Yubo Cao <cao2006721@gmail.com>
2022-12-29 18:58:54 -0500

    feat: thicker active ring

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -15,14 +15,14 @@ export default function MenuButton({
     let [entered, setEntered] = useState(false);
 
     return (
-        <div
+        <button
             onClick={onClick}
             onMouseEnter={() => setEntered(true)}
             onMouseLeave={() => setEntered(false)}
             className={cls(
                 "transition-all",
                 "rounded-full",
-                "active:border",
+                "active:border-2",
                 "active:border-black",
                 "active:bg-slate-100",
                 "hover:bg-slate-100",
@@ -40,6 +40,6 @@ export default function MenuButton({
                 className="transition-all"
                 grade={entered ? 200 : 0}
             ></Icon>
-        </div>
+        </button>
     );
 }

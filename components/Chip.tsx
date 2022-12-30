34486415b14c5b67c9aec139dedd7a0287cd24e6
Yubo Cao <cao2006721@gmail.com>
2022-12-29 19:20:49 -0500

    fix(Chip): bring chip component back

---
+++ components/Chip.tsx
@@ -0,0 +1,29 @@
+import { Component, ReactNode } from "react";
+
+export class Chip extends Component<{ children: ReactNode; onClick?: () => any }> {
+    render() {
+        return (
+            <button
+                className={[
+                    "px-2.5",
+                    "py-0.5",
+                    "rounded-lg",
+                    "font-base",
+                    "text-sm",
+                    "text-slate-500",
+                    "cursor-pointer",
+                    "border",
+                    "border-primary-200",
+                    "hover:bg-primary-100",
+                    "active:ring-2",
+                    "active:ring-primary-200",
+                    "active:ring-opacity-50",
+                    "active:bg-primary-100"
+                ].join(" ")}
+                onClick={this.props.onClick}
+            >
+                {this.props.children}
+            </button>
+        );
+    }
+}

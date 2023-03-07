5f5662b764e7e29cdbf451e0ade8e461ecaeb369
Yubo-Cao <cao2006721@gmail.com>
2023-03-06 23:00:58 -0500

    feat: pure CSS implementation of banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,121 +1,29 @@
 import React from "react";
-import { currentBreakpoint } from "../lib/breakpoints";
-import { cls } from "../lib/utils";
+import styles from "./Banner.module.css";
 
 type BannerProps = {
     children: React.ReactNode;
     className?: string;
-    bannerClassName?: string;
     style?: React.CSSProperties;
-    bannerStyle?: React.CSSProperties;
-    stick?: "left" | "right" | "both" | "none";
+    stick?: "left" | "right" | "both";
+    variant?: any;
     avoidTOC?: boolean;
 };
 
-type BannerState = {
-    needAvoid: boolean;
-};
-
-// rewrite as class
-export default class Banner extends React.Component<BannerProps, BannerState> {
-    ref: React.RefObject<HTMLDivElement>;
-    stick: "left" | "right" | "both" | "none";
-    avoidTOC: boolean;
-
-    constructor(props: BannerProps) {
-        super(props);
-        this.ref = React.createRef();
-        this.stick = props.stick || "both";
-        this.setState({ needAvoid: false });
-        this.avoidTOC = Object.hasOwn(props, "avoidTOC")
-            ? props.avoidTOC!!
-            : true;
-        this.widthAdjuster = this.widthAdjuster.bind(this);
-    }
-
-    componentDidMount() {
-        this.widthAdjuster();
-        window.addEventListener("resize", this.widthAdjuster);
-        window.addEventListener("scroll", this.widthAdjuster);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.widthAdjuster);
-        window.removeEventListener("scroll", this.widthAdjuster);
-    }
-
-    widthAdjuster() {
-        const { current } = this.ref;
-        if (!current) return;
-        const parent = current.parentElement;
-        if (!parent) return;
-        const rect = parent.getBoundingClientRect();
-
-        if (
-            (this.stick === "left" || this.stick === "both") &&
-            !Object.hasOwn(this.props.bannerStyle || {}, "left")
-        )
-            current.style.left = `-${parent.offsetLeft}px`;
-        if (
-            (this.stick === "right" || this.stick === "both") &&
-            !Object.hasOwn(this.props.bannerStyle || {}, "right")
-        )
-            current.style.right = `-${
-                window.innerWidth - parent.offsetLeft - parent.offsetWidth
-            }px`;
-        if (
-            !Object.hasOwn(this.props.bannerStyle || {}, "top") &&
-            !Object.hasOwn(this.props.bannerStyle || {}, "bottom")
-        ) {
-            current.style.top = `${rect.top}px`;
-            current.style.bottom = `${window.innerHeight - rect.bottom}px`;
-        }
-
-        if (this.avoidTOC && currentBreakpoint(window.innerWidth) === "2xl") {
-            current.style.position = "absolute";
-            current.style.right = "0";
-            current.style.top = "0";
-            current.style.bottom = "0";
-            current.style.borderTopRightRadius = "1rem";
-            current.style.borderBottomRightRadius = "1rem";
-            current.parentElement!!.style.paddingRight = "2rem";
-        } else {
-            current.style.position = "fixed";
-            current.parentElement!!.style.paddingRight = "0";
-            current.style.borderTopRightRadius = "0";
-            current.style.borderBottomRightRadius = "0";
-        }
-    }
-
-    render() {
-        const {
-            children,
-            className = "",
-            bannerClassName = "",
-            style
-        } = this.props;
-
-        return (
-            <div
-                className={cls(
-                    "relative",
-                    /\bp[xy]-\d+\b/.test(className) ? "" : "py-2",
-                    className
-                )}
-                style={style}
-            >
+export default function Banner({
+    children,
+    className,
+    style,
+    stick = "both",
+    avoidTOC = true,
+    variant = "gradient",
+}: BannerProps) {
+    return (
+        <>
+            <div className={styles.container}>
                 {children}
-                <div
-                    className={cls(
-                        "fixed",
-                        bannerClassName ||
-                            "bg-gradient-to-r from-primary-100 to-secondary-100",
-                        "-z-10"
-                    )}
-                    style={this.props.bannerStyle}
-                    ref={this.ref}
-                />
+                <div className={`${avoidTOC && styles.avoid} ${styles.base} ${styles[stick]} ${styles[variant] || `bg-${variant}`} ${className} py-6`} style={style} />
             </div>
-        );
-    }
+        </>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,33 @@
 import Image from "next/image";
-import HeaderLayout from "../components/HeaderLayout";
-import Section from "../components/Section";
+import { HeaderLayout, Banner } from "@/components";
 
 export default function Index() {
     return (
         <HeaderLayout active={"Home"}>
-            <Section
-                className="mx-auto max-w-4xl"
-                contentClassName="flex gap-4 items-center justify-center max-md:flex-col"
-                alternate="none"
-            >
-                <Image
-                    src="/images/about/yubo.png"
-                    alt="Yubo’s Figure"
-                    width={200}
-                    height={300}
-                    className="grayscale basis-12 flex-auto max-h-96 object-contain object-left drop-shadow-2xl"
-                    style={{ flexGrow: 1 }}
-                />
-                <div
-                    className="flex flex-col basis-96 flex-1 md:mx-16 max-md:basis-16 prose "
-                    style={{ flexGrow: 3 }}
-                >
-                    <h1>Yubo Cao</h1>
-                    <p className="text-lg mt-2">
-                        I am a motivated sophomore with a passion for
-                        programming and artificial intelligence, excited to
-                        continue learning and striving for excellence.
-                    </p>
+
+            <Banner>
+                <div className="flex gap-16 items-center max-w-4xl mx-auto">
+                    <Image
+                        src="/images/about/yubo.png"
+                        alt="Yubo’s Figure"
+                        width={200}
+                        height={300}
+                        className="grayscale basis-12 flex-auto max-h-96 object-contain object-left drop-shadow-2xl"
+                        style={{ flexGrow: 1 }}
+                    />
+                    <div
+                        className="basis-96 md:mx-16 max-md:basis-16"
+                        style={{ flexGrow: 3 }}
+                    >
+                        <h1>Yubo Cao</h1>
+                        <p className="text-lg mt-2">
+                            I am a motivated sophomore with a passion for
+                            programming and artificial intelligence, excited to
+                            continue learning and striving for excellence.
+                        </p>
+                    </div>
                 </div>
-            </Section>
+            </Banner>
         </HeaderLayout>
     );
 }
